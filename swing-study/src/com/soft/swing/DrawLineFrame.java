5c3e1ffa18af0f9ce6cd2ed171e6f569afb45499
ZGQ1999 <2303771402@qq.com>
2019-04-10 06:33:03 +0800

    绘图线程小练习

---
+++ swing-study/src/com/soft/swing/DrawLineFrame.java
@@ -0,0 +1,26 @@
+package com.soft.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawLineFrame extends JFrame {
+    public DrawLineFrame(){
+        init();
+        setTitle("绘制彩色线段");
+        setSize(900,600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    private void init(){
+        getContentPane().setBackground(new Color(0,0,0));
+        DrawLineThread drawLineThread = new DrawLineThread();
+        drawLineThread.setFrame(this);
+        new Thread(drawLineThread).start();
+    }
+
+    public static void main(String[] args) {
+        new DrawLineFrame();
+    }
+
+}
